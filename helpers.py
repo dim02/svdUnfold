837b19dbdd12646c74fa69279fa77b3178599013
Dimitrios Palioselitis <dim.palioselitis@gmail.com>
2020-04-25 08:55:32 +0200

    Test construction of second derivative matrix

--- helpers.py
+++ helpers.py
@@ -5,3 +5,17 @@ import numpy as np
 def check_symmetric(matrix, rtol=1e-05, atol=1e-08):
     """Return true if matrix is symmetric"""
     return np.allclose(matrix, matrix.T, rtol=rtol, atol=atol)
+
+
+def calc_second_deriv_matrix(n, xi):
+    """Return n by n second derivative matrix with small diagonal xi"""
+    c_matrix = np.zeros((n, n))
+    for i in range(n):
+        c_matrix[i, i] = -2 + xi
+        if i - 1 >= 0:
+            c_matrix[i, i - 1] = 1
+        if i + 1 < n:
+            c_matrix[i, i + 1] = 1
+    c_matrix[0, 0] = -1 + xi
+    c_matrix[n - 1, n - 1] = -1 + xi
+    return c_matrix
--- tests/test_svdunfold.py
+++ tests/test_svdunfold.py
@@ -4,6 +4,7 @@ A collection of tests for svdunfold
 import pytest
 import numpy as np
 import svdunfold
+import helpers
 
 
 def test_exception_for_rows_in_response_matrix():
@@ -39,3 +40,35 @@ def test_exception_for_symmetric_covariance():
     cov[0, 1] = 1
     with pytest.raises(AssertionError, match=r".*is not symmetric.*"):
         svdunfold.SVDunfold(x_ini, b, A, cov)
+
+
+def test_contruct_c_matrix_3d():
+    """Test if 3d second derivative matrix is constructed correctly"""
+    c_matrix = np.array([[-1, 1, 0], [1, -2, 1], [0, 1, -1]])
+    assert np.array_equal(c_matrix, helpers.calc_second_deriv_matrix(3, 0))
+
+
+def test_contruct_c_matrix_7d():
+    """Test if 7d second derivative matrix is constructed correctly"""
+    c_matrix = np.array([[-1, 1, 0, 0, 0, 0, 0],
+                         [1, -2, 1, 0, 0, 0, 0],
+                         [0, 1, -2, 1, 0, 0, 0],
+                         [0, 0, 1, -2, 1, 0, 0],
+                         [0, 0, 0, 1, -2, 1, 0],
+                         [0, 0, 0, 0, 1, -2, 1],
+                         [0, 0, 0, 0, 0, 1, -1]])
+    assert np.array_equal(
+        c_matrix, helpers.calc_second_deriv_matrix(7, 0))
+
+
+def test_contruct_c_matrix_7d_with_xi():
+    """Test if 7d second derivative matrix with x=0.1 is constructed correctly"""
+    c_matrix = np.array([[-0.9, 1, 0, 0, 0, 0, 0],
+                         [1, -1.9, 1, 0, 0, 0, 0],
+                         [0, 1, -1.9, 1, 0, 0, 0],
+                         [0, 0, 1, -1.9, 1, 0, 0],
+                         [0, 0, 0, 1, -1.9, 1, 0],
+                         [0, 0, 0, 0, 1, -1.9, 1],
+                         [0, 0, 0, 0, 0, 1, -0.9]])
+    assert np.array_equal(
+        c_matrix, helpers.calc_second_deriv_matrix(7, 0.1))
